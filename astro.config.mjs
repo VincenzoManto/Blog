b29faa6d15ab8e58dfe756450e5698269aaa67ee
VincenzoManto <vincmanto@gmail.com>
2026-06-02 18:16:36 +0200

    Add Fracta Language Specification and OEF Standard documentation
    - Introduced a new document for the Fracta Language Specification detailing its grammar, lexical structure, and execution semantics.
    - Added an article for the Online Encyclopedia of Fractals (OEF) Standard, outlining its data schema, meta-syntax, and execution core.
    - Both documents include comprehensive sections on architectural principles, protocol syntax, and examples to facilitate understanding and implementation.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
   output: 'static',
+    markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
   base: '/Blog/', // Cambia 'Blog' con il nome del repo se diverso
 });
